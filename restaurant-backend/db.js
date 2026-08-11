8f5d8dbecd2b35a9c2004fa70bf8b51fd901ad8c
likith <likith.21bce8536@vitapstudent.ac.in>
2026-08-11 08:13:33 +0530

    Update MySQL connection for cloud database

--- restaurant-backend/db.js
+++ restaurant-backend/db.js
@@ -4,6 +4,7 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,//aiven added
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -11,6 +12,9 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl: {
+  rejectUnauthorized: false,//aiven added
+},
 });
 
 pool.getConnection((err, connection) => {
