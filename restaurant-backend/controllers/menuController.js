6e1627ebcd76c8e4f81c004ed5e4ef017d858728
likith <likith.21bce8536@vitapstudent.ac.in>
2026-08-12 08:27:40 +0530

    show menu database error

--- restaurant-backend/controllers/menuController.js
+++ restaurant-backend/controllers/menuController.js
@@ -1,19 +1,20 @@
 const menuModel = require("../models/menuModel");
 
-const getMenu = (req, res) => {
-    console.log("Menu route hit", req.url);
-    menuModel.getMenu((err, results) => {
-        if (err) {
-            console.error("Menu query error", err);
-            return res.status(500).json({
-                message: "Error fetching menu"
-            });
-        }
+const getMenu = async (req, res) => {
+  try {
+    const menu = await menuModel.getAllMenu();
 
-        res.json(results);
+    res.status(200).json(menu);
+  } catch (error) {
+    console.error("MENU DATABASE ERROR:", error);
+
+    res.status(500).json({
+      message: "Error fetching menu",
+      error: error.message,
     });
+  }
 };
 
 module.exports = {
-    getMenu
+  getMenu,
 };
